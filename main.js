7aaef2b29f86f3c18578026403e3cf54812a9f32
PulatovSO <pulatovsirotillo@gmail.com>
2021-12-17 10:04:07 +0500

    changed style

--- main.js
+++ main.js
@@ -1,33 +1,51 @@
-
 let x = document.querySelector('.x');
 let y = document.querySelector('.y');
-let blurEffect = document.querySelector('.blur');
+let blurr = document.querySelector('.blur');
 let spread = document.querySelector('.spread');
+
 let result = document.querySelector('.result');
 let code = document.querySelector('.code');
+let resetBtn = document.querySelector('.reset');
+
+let valueX = document.querySelector('.x-value');
+let valueY = document.querySelector('.y-value');
+let valueBlur = document.querySelector('.blur-value');
+let valueSpread = document.querySelector('.spread-value');
+
+x.oninput = (e) => {
+  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px white`
+  valueX.textContent = `${x.value}px`;
+  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px #ffffff;`
+}
+
+y.oninput = (e) => {
+  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px white`
+  valueY.textContent = `${y.value}px`;
+  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px #ffffff;`
+}
+
+blurr.oninput = (e) => {
+  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px white`
+  valueBlur.textContent = `${blurr.value}px`;
+  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px #ffffff;`
+}
+
+spread.oninput = (e) => {
+  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px white`
+  valueSpread.textContent = `${spread.value}px`;
+  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px #ffffff;`
+}
 
 
-x.addEventListener('keyup', () => {
-  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px white`
-  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px #ffffff;`
+resetBtn.addEventListener('click', () => {
+  x.value = 0;
+  y.value = 0;
+  blurr.value = 0;
+  spread.value = 0;
+  valueY.textContent = '0px';
+  valueX.textContent = '0px';
+  valueBlur.textContent = '0px';
+  valueSpread.textContent = '0px';
+  result.style['box-shadow'] = `0 0 0 0 white`
+  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurr.value}px ${spread.value}px #ffffff;`
 })
-
-y.addEventListener('keyup', () => {
-  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px white`
-  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px #ffffff;`
-})
-
-blurEffect.addEventListener('keyup', () => {
-  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px white`
-  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px #ffffff;`
-})
-
-spread.addEventListener('keyup', () => {
-  result.style['box-shadow'] = `${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px white`
-  code.textContent = `box-shadow: ${x.value}px ${y.value}px ${blurEffect.value}px ${spread.value}px #ffffff;`
-})
-
-
-
-
-
